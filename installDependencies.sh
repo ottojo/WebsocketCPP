169b08229fb5fd0d8464d3e4879f90a760fefa98
CarolinS <carolingit@t-online.de>
2020-04-22 10:02:44 +0200

    added installDependencies for submodule use

---
+++ installDependencies.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Setup
+# Exit the script if any command fails
+set -e
+
+
+# Libwebsockets
+sudo apt install libssl-dev
+cd /tmp
+git clone https://github.com/warmcat/libwebsockets.git
+cd libwebsockets
+mkdir build && cd build
+cmake -DJSON_BuildTests=false ..
+make -j$(nproc)
+sudo make install
+
+
+sudo ldconfig
